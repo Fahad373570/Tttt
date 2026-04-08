7cd8a3f0988292af5b21bfdbeda7dba501ae8b7e
Fahad373570 <mfahad99990@gmail.com>
2026-04-08 10:40:51 +0500

    Add files via upload

---
+++ src/components/stage_one.jsx
@@ -0,0 +1,91 @@
+import { useState, useContext, useRef } from "react";
+import { Button, Form, Alert } from "react-bootstrap";
+
+import { MyContext } from "../context";
+const Stage1=()=>{
+const textInput = useRef();
+const context = useContext(MyContext);
+const [error , setError]= useState([false,''])
+
+
+const handleSubmit = (e) =>{
+    e.preventDefault();
+    const value = textInput.current.value;
+    const validate = validateInput(value);
+//Valition
+
+if(validate){
+    setError([false,''])
+context.addPlayer(value);
+textInput.current.value ='';
+    // console.log(value);
+}
+
+
+}
+
+const validateInput = (value)=>{
+    if(value === ''){
+        setError([true,'Sorry, you need to add something'])
+        return false
+    }
+    if(value.length<= 2){
+        setError([true,'Sorry, you need 3 char at least'])
+        return false;
+    }
+    return true;
+
+}
+
+    return(
+        <>
+        <Form onSubmit={handleSubmit} className="mt-4">
+<Form.Group>
+    <Form.Control
+    type="text"
+    placeholder="Add player name"
+    name="player"
+    ref={textInput}
+    />
+</Form.Group>
+{/* error */}
+
+{error[0] ?
+<Alert>
+    {error[1]}
+</Alert>:null
+}
+
+<Button className="miami" variant="primary" type="submit" >
+Add Player
+</Button>
+{context.players && context.players.length > 0 ?
+<>
+<hr />
+<div>
+    <ul className="list-group">
+        {context.players.map((players,idx)=>(
+            <li key={idx} className="list-group-item
+             d-flex justify-content-between 
+             align-items-center
+              list-group-item-action">
+                {players}
+                <span className="badge badge-danger" onClick={()=>context.removePlayer(idx)}>
+X
+                </span>
+
+            </li>
+        ))}
+    </ul>
+</div>
+<div className="action_button" onClick={()=> context.next()}>
+NEXT
+</div>
+</>
+:null}
+
+        </Form>
+        </>
+    )
+}
+export default Stage1; 
---
+++ src/components/stage_two.jsx
@@ -0,0 +1,23 @@
+import { useContext } from "react";
+import { MyContext } from "../context";
+
+
+const Stage2=()=>{
+const context= useContext(MyContext)
+
+    return(
+        <>
+        <div className="result_wrapper">
+            <h3>The loser is:</h3>
+            {context.result}
+        </div>
+        <div className="action_button" onClick={()=> context.resetGame()}>
+            Start Over
+        </div>
+        <div className="action_button btn_2" onClick={()=> context.generateNewLoser()}>
+            Get new Loser
+        </div>
+        </>
+    )
+}
+export default Stage2; 
